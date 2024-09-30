dfa754e4648f3caf4a9fd5a6d1805d9c7cef8eec
grusso98 <sezc.gianmarco.russo@gmail.com>
2024-09-30 12:35:08 +0200

    defined collision detection and the remaining classes

--- shape.py
+++ shape.py
@@ -1,43 +1,47 @@
-import numpy as np 
+
+import numpy as np
+
+
 class Shape:
     def __init__(self, rotations):
         self.rotations = rotations
-        self.num_rotations = len(rotations)
-    
+        self.rotations_count = len(rotations)
+
     def get_matrix_with_offset(self, rotation, offset):
         return offset + self.rotations[rotation]
 
+
 def generate_shapes():
-    #"I" shape
+    # shape i
     shape_i_rot1 = np.array([[1, 0], [1, 1], [1, 2], [1, 3]], np.int32)
     shape_i_rot2 = np.array([[0, 1], [1, 1], [2, 1], [3, 1]], np.int32)
     shape_i = Shape(np.array([shape_i_rot1, shape_i_rot2]))
 
-    #"L" shape
+    # shape l
     shape_l_rot1 = np.array([[1, 0], [1, 1], [1, 2], [2, 2]], np.int32)
     shape_l_rot2 = np.array([[0, 2], [1, 2], [2, 2], [2, 1]], np.int32)
     shape_l_rot3 = np.array([[1, 0], [2, 0], [2, 1], [2, 2]], np.int32)
     shape_l_rot4 = np.array([[0, 3], [0, 2], [1, 2], [2, 2]], np.int32)
     shape_l = Shape(np.array([shape_l_rot1, shape_l_rot2, shape_l_rot3, shape_l_rot4]))
 
-    #"j" shape 
+    # shape j
     shape_j_rot1 = np.array([[2, 0], [2, 1], [2, 2], [1, 2]], np.int32)
     shape_j_rot2 = np.array([[0, 1], [1, 1], [2, 1], [2, 2]], np.int32)
     shape_j_rot3 = np.array([[3, 0], [2, 0], [2, 1], [2, 2]], np.int32)
     shape_j_rot4 = np.array([[0, 1], [0, 2], [1, 2], [2, 2]], np.int32)
     shape_j = Shape(np.array([shape_j_rot1, shape_j_rot2, shape_j_rot3, shape_j_rot4]))
 
-    #"s" shape
+    # shape_s
     shape_s_rot1 = np.array([[0, 2], [1, 2], [1, 1], [2, 1]], np.int32)
     shape_s_rot2 = np.array([[1, 0], [1, 1], [2, 1], [2, 2]], np.int32)
     shape_s = Shape(np.array([shape_s_rot1, shape_s_rot2]))
 
-    #"z" shape
+    # shape_z
     shape_z_rot1 = np.array([[0, 1], [1, 1], [1, 2], [2, 2]], np.int32)
     shape_z_rot2 = np.array([[2, 0], [2, 1], [1, 1], [1, 2]], np.int32)
     shape_z = Shape(np.array([shape_z_rot1, shape_z_rot2]))
 
-    #"t" shape
+    # shape_t
     shape_t_rot1 = np.array([[0, 2], [1, 2], [2, 2], [1, 1]], np.int32)
     shape_t_rot2 = np.array([[2, 0], [2, 1], [2, 2], [1, 1]], np.int32)
     shape_t_rot3 = np.array([[0, 1], [1, 1], [2, 1], [1, 2]], np.int32)
@@ -48,6 +52,7 @@ def generate_shapes():
     shape_o = Shape(np.array([shape_o_rot1]))
 
     return list([shape_i, shape_l, shape_j, shape_s, shape_z, shape_t, shape_o])
+
 
 def generate_colours():
     RED = [255, 0, 0]
--- tetris.py
+++ tetris.py
@@ -1,20 +1,49 @@
 import pygame.display
+
 from board import Board
 
-class Tetris:
-    def __init__(self):
-        self.screen =  pygame.display.set_mode((720,1280))
-        self.board = Board(self.screen)
-        self.running = True
-        self.speed = 10
-        self.clock = pygame.time.Clock()
-        self.run()
-    
-    def run(self):
-        while self.running == True:
-            self.screen.fill("Black")
-            pygame.display.flip()
-            self.board.draw()
-        pygame.quit()
 
-Tetris().run()
+class Tetris:
+
+    def __init__(self):
+        pygame.init()
+        self._screen = pygame.display.set_mode((540, 680))
+        self._clock = pygame.time.Clock()
+        self._running = True
+        self._speed = 40
+        self._board = Board(self._screen)
+        pygame.font.init()
+        self._score_font = pygame.font.SysFont('Arial', 20)
+        self.run()
+
+    def run(self):
+        counter = 0
+        while self._running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self._board.on_key_up()
+                    if event.key == pygame.K_DOWN:
+                        self._board.on_key_down()
+                    if event.key == pygame.K_LEFT:
+                        self._board.on_key_left()
+                    if event.key == pygame.K_RIGHT:
+                        self._board.on_key_right()
+                    self._screen.fill("black")
+                    self._board.update(False)
+                    self._board.draw()
+
+            if counter % self._speed == 0:
+                self._board.update()
+                counter = 1
+                self._screen.fill("black")
+                self._board.draw()
+
+            pygame.display.flip()
+            counter += 1
+            self._clock.tick(40)
+            text_surface = self._score_font.render('Score: ' + str(self._board.score), False, (255, 255, 255))
+            self._screen.blit(text_surface, (500, 150))
+        pygame.quit()
